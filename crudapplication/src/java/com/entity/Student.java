511485cf20c4d4e99cfb390dbd19c541ff30b5fa
Mahesh-Bisunke <maheshbisunke92@gmail.com>
2025-07-30 18:08:55 +0545

    Javax annotation api Added

--- crudapplication/src/java/com/entity/Student.java
+++ crudapplication/src/java/com/entity/Student.java
@@ -1,8 +1,17 @@
 
 package com.entity;
 
+import javax.persistence.*;
 
+@Entity
 public class Student {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String address;
+    private String studentClass;
+    private String subject;
     
 }
